b014df2148e2c245ca58d06aca6fe37250838dc2
MaxMillington <maxmillington89@gmail.com>
2015-07-30 13:55:02 -0600

    added all of the belongs to and has many relationships to the models

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,2 +1,6 @@
 class Reward < ActiveRecord::Base
+  has_many :user_rewards
+  has_many :users, through: :user_rewards
+
+  validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  has_many :user_rewards
+  has_many :rewards, through: :user_rewards
+  has_secure_password
+
+  validates :username, presence: true,
+            uniqueness: true
+
+  enum role: %w(default admin)
 end
